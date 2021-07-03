bd791bd4326aebc79682699edbd3d684bfd081d2
Evelyn Monteiro <evelynsilmonteiro@gmail.com>
2021-07-03 09:45:08 -0300

    Finalização do projeto

--- app.js
+++ app.js
@@ -1,85 +1,144 @@
-const Storage = {
-    get(){
-        return JSON.parse(localStorage.getItem("eveTodo:tasks")) || []
-    },
-    set(tasks){
-        localStorage.setItem("eveTodo:tasks", JSON.stringify(tasks))
+/* Selectors */
+
+const todoInput = document.querySelector('.todo-input')
+const addButton = document.querySelector('.add')
+const cardsContainer = document.querySelector('.cards')
+const filterOption = document.querySelector('.filter-todos')
+
+/* Event Listeners */
+
+document.addEventListener('DOMContentLoaded', getTodos);
+addButton.addEventListener('click', addTask);
+cardsContainer.addEventListener('click', deleteAndCheckTask);
+filterOption.addEventListener('click', filterTodo);
+
+
+/* functions */
+
+function addTask(event){
+    event.preventDefault();
+
+    /*create new item*/
+
+    const htmlCard = `
+        <div class="text">${todoInput.value}</div>
+        <button class="done">
+            <i class="fa fa-check" aria-hidden="true"></i>
+        </button>
+        <button class="delete">
+            <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+    `
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = htmlCard;
+    cardsContainer.appendChild(card);
+
+    /*add to a LocalStorage*/
+
+    saveLocalTodos(todoInput.value);
+
+    /*clear to-do input value*/
+
+    todoInput.value = "";
+}
+
+function deleteAndCheckTask(e){
+    const item = e.target;
+
+    //Delete
+
+    if(item.classList[0] === 'delete'){
+        const todo = item.parentElement;
+        removeLocalTodos(todo);
+        todo.remove();
+    }
+
+    //Check
+
+    if(item.classList[0] === 'done'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 }
 
-const Card = {
-    all: Storage.get(),
-    add(task){
-        Card.all.push(task);
-        App.reload();
-    },
-    remove(index){
-        Card.all.splice(index, 1);
-        App.reload();
-    }
-}
+function filterTodo(e){
+    const todos = cardsContainer.childNodes;
 
-const DOM = {
-    cardContainer: document.querySelector('.cards'),
-    addCard(task, index){
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = DOM.innerHTMLCard(task, index);
-        card.dataset.index = index;
-        DOM.cardContainer.appendChild(card);
-    },
-    innerHTMLCard(task, index){
-        const html = `
-            <div class="text">${task.description}</div>
-            <button class="done"><i class="fa fa-check" aria-hidden="true"></i></button>
-            <button class="delete" onclick="Card.remove(${index})"><i class="fa fa-trash" aria-hidden="true"></i></button> 
-        `
-        return html;
-    },
-    clearCards(){
-        DOM.cardContainer.innerHTML = "";
-    }
-}
-
-const Form = {
-    description: document.querySelector('input#todoInput'),
-
-    getValues(){
-        return {
-            description: Form.description.value
+    
+    todos.forEach(function(item){
+        switch(e.target.value){
+            case "all":
+                item.style.display = "flex";
+                break;
+            case "completed":
+                if(item.classList.contains("completed")){
+                    item.style.display = "flex";
+                }else{
+                    item.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!item.classList.contains("completed")){
+                    item.style.display = "flex";
+                }else{
+                    item.style.display = "none";
+                }
+                break;
         }
-    },
-    validateField(){
-        const { description } = Form.getValues();
-        if(description.trim() === ""){
-            throw new Error("Preencha o campo");
-        }
-    },
-    clearField(){
-        Form.description.value = ""
-    },
-    submit(event){
-        event.preventDefault();
-
-        try{
-            Form.validateField();
-            Card.add(this.getValues());
-            Form.clearField();
-        }catch(error){
-            alert(error.message);
-        }       
-    }
+    })
 }
 
-const App = {
-    init(){
-        Card.all.forEach(DOM.addCard)
-        Storage.set(Card.all);
-    },
-    reload(){
-        DOM.clearCards();
-        App.init()
+function saveLocalTodos(todo){
+    let todos;
+
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
     }
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-App.init();
+function getTodos(){
+    let todos;
+
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach(function(item){
+        const htmlCard = `
+        <div class="text">${item}</div>
+        <button class="done">
+            <i class="fa fa-check" aria-hidden="true"></i>
+        </button>
+        <button class="delete">
+            <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+    `
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = htmlCard;
+    cardsContainer.appendChild(card);
+    });
+}
+
+function removeLocalTodos(todo){
+    let todos;
+
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos))
+    
+}
